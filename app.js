8985dd57444ffaa812f65a38f3acc51f72fcd75e
Salieri <defaputra505@gmail.com>
2026-03-10 10:32:45 +0700

    Add simple Node.js app with greeting function

---
+++ app.js
@@ -0,0 +1,10 @@
+// Simple Node.js App
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to this simple Node.js project.`;
+}
+
+const name = "Defa Augista Montaine Dhasnarto Putera";
+
+console.log("Program berjalan...");
+console.log(greet(name));
